1ce2978dc50826561188865e8044cd1d110a01bc
qwertyuiiooopppppp <247wga@naver.com>
2023-04-05 14:06:28 +0900

    1

---
+++ src/ex01/Exam01.java
@@ -0,0 +1,15 @@
+package ex01;
+
+public class Exam01 {
+
+	public static void main(String[] args) {
+        
+		boolean power = true; // true, false
+		
+		if(power) // () 안 내용이 참이면 10줄 실행, () 안 내용이 거짓이면 12줄 실행 
+			 System.out.println("power는 true");
+		else
+			 System.out.println("power는 false");	
+     }
+	
+}
